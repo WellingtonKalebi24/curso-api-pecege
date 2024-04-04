e487a3271de083734004cca2c305043d28cec628
Wellington Joia <wellington.kalebi@outlook.com>
2024-04-03 22:08:29 -0300

    Adicionando valitador no shelter input

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty, IsNumberString, IsString, Length } from "class-validator"
+
 export default class UpdateShelterControllerInput {
+    @IsString()
+    @IsNotEmpty()
     name: string
+    @IsString()
+    @Length(10,11)
+    @IsNotEmpty()
     whatsapp: string
+    @IsNumberString()
+    @IsNotEmpty()
     phone: string
+    @IsEmail()
+    @IsNotEmpty()
     email: string
 }
